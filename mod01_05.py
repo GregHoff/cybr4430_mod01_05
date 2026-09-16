1cdc1c8bd36d41bca28ccbca3fb159fd559282e2
Greg Hoff <31416820+GregHoff@users.noreply.github.com>
2026-09-16 11:47:55 -0500

    Update mod01_05.py

---
+++ mod01_05.py
@@ -0,0 +1,24 @@
+# function that takes a matrix as its input and returns the number of dimensions if a well-formed matrix and Linear Independent, otherwise return False.
+def checkLinearIndependence(m1):
+    #Complete the function
+    return False
+
+# function that takes a matrix as its input and returns the number of dimensions if a well-formed identity matrix, otherwise return False.
+def checkIdentity(m1):
+    #Complete the function
+    return False
+
+# function that takes a matrix as its input and returns the number of dimensions if a well-formed Hadamard matrix, otherwise return False.
+def checkHadmond(m1):
+    #Complete the function
+    return False
+
+# function that returns identiy matrix in n dimensions
+def getIdentity(n):
+    #Complete the function
+    return False
+
+# function that returns Hadamard matrix in n dimensions
+def getHadmond(n):
+    #Complete the function
+    return False
